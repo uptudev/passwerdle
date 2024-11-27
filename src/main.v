6c1e7735dd8a7ad244a402dd6fe72e328580493f
uptu <uptu@uptu.dev>
2024-11-26 18:27:57 -0800

    Doc typo

--- src/main.v
+++ src/main.v
@@ -1,6 +1,6 @@
 /*
  *  Passwerdle, a puzzle game based off of guessing common passwords
- *  Copyright (C) 2024	uptu <uptu@uptu.dev>
+ *  Copyright (C) 2024 uptu <uptu@uptu.dev>
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
